48a7b8af42d4c802e142c04ff8764745c0072c69
Chris Maughan <120447+cmaughan@users.noreply.github.com>
2025-07-30 17:22:35 +0100

    Fixe

--- main.cpp
+++ main.cpp
@@ -21,12 +21,12 @@ extern "C" {
 using namespace pico_ssd1306;
 
 // Utility function to blind the light for debugging
-void blink()
+void blink(uint32_t count)
 {
     gpio_put(PICO_DEFAULT_LED_PIN, 1);
-    sleep_ms(500);
+    sleep_ms(count);
     gpio_put(PICO_DEFAULT_LED_PIN, 0);
-    sleep_ms(500);
+    sleep_ms(count);
 }
 
 // Rotary encoder connections
@@ -41,23 +41,17 @@ void blink()
 std::atomic<int> encoder_count = 0; // Counter for the encoder position
 std::atomic<bool> button_pressed = false;
 std::atomic<bool> button_state = false;
-    
-std::atomic<uint64_t> value = 7000000;
 
+uint64_t frequency = 7000000;
 // Get the encoder state
-uint8_t enc_state(void)
+uint8_t read_encoder_state(void)
 {
-    static uint8_t prev_state = 0;
-
-    uint8_t new_state = gpio_get(ENCODER_DT) + (2 * gpio_get(ENCODER_CLK));
-    return new_state;
+    return (gpio_get(ENCODER_DT) > 0 ? 1 : 0) | ((gpio_get(ENCODER_CLK) > 0 ? 1 : 0) << 1);
 }
 
 // Handle the encoder switch
 long long int handle_switch(long int a, void* p)
 {
-    value++;
-    button_pressed = true;
     if (gpio_get(ENCODER_SWITCH) == 1 && button_state == false)
     {
         // Trigger button press
@@ -76,18 +70,16 @@ void encoder_callback(uint gpio, uint32_t events)
 {
     if (gpio == ENCODER_SWITCH)
     {
-        value++;
         // Debounce the switch
         add_alarm_in_ms(50, handle_switch, nullptr, true);
     }
     else if (gpio == ENCODER_CLK || gpio == ENCODER_DT)
     {
-        value++;
         // Track the pulses on the encoder and turn into a sensible rotary count.
         static uint8_t saved_enc = 0;
         uint8_t enc_now, enc_prev;
 
-        enc_now = enc_state();
+        enc_now = read_encoder_state();
         if (enc_now == saved_enc)
         {
             return;
@@ -101,7 +93,7 @@ void encoder_callback(uint gpio, uint32_t events)
         {
             encoder_count++;
         }
-        if ((enc_prev == 3 && enc_now == 2) || (enc_prev == 2 && enc_now == 0) || (enc_prev == 0 && enc_now == 1) || (enc_prev == 1 && enc_now == 3))
+        else if ((enc_prev == 3 && enc_now == 2) || (enc_prev == 2 && enc_now == 0) || (enc_prev == 0 && enc_now == 1) || (enc_prev == 1 && enc_now == 3))
         {
             encoder_count--;
         }
@@ -123,32 +115,31 @@ int main()
 
     gpio_set_dir(DISPLAY_CLOCK, GPIO_IN);
     gpio_set_dir(DISPLAY_DATA, GPIO_IN);
-    // Rotary encoder
-    //gpio_set_function(ENCODER_SWITCH, GPIO_FUNC_SIO);
-    //gpio_set_function(ENCODER_CLK, GPIO_FUNC_SIO);
-    //gpio_set_function(ENCODER_DT, GPIO_FUNC_SIO);
 
-    /*
+    // Rotary encoder
+    gpio_set_function(ENCODER_SWITCH, GPIO_FUNC_SIO);
+    gpio_set_function(ENCODER_CLK, GPIO_FUNC_SIO);
+    gpio_set_function(ENCODER_DT, GPIO_FUNC_SIO);
+
     gpio_set_dir(ENCODER_CLK, GPIO_IN);
     gpio_set_dir(ENCODER_DT, GPIO_IN);
     gpio_set_dir(ENCODER_SWITCH, GPIO_IN);
-    */
 
-    /*
     gpio_pull_up(ENCODER_CLK);
     gpio_pull_up(ENCODER_DT);
     gpio_pull_up(ENCODER_SWITCH);
-    */
 
-    //gpio_set_irq_enabled_with_callback(ENCODER_DT, GPIO_IRQ_EDGE_FALL | GPIO_IRQ_EDGE_RISE, true, &encoder_callback);
-    //gpio_set_irq_enabled(ENCODER_CLK, GPIO_IRQ_EDGE_FALL | GPIO_IRQ_EDGE_RISE, true);
-    //gpio_set_irq_enabled(ENCODER_SWITCH, GPIO_IRQ_EDGE_FALL | GPIO_IRQ_EDGE_RISE, true);
+    // gpio_set_irq_enabled_with_callback(ENCODER_DT, GPIO_IRQ_EDGE_FALL /*| GPIO_IRQ_EDGE_RISE*/, true, &encoder_callback);
+    // gpio_set_irq_enabled(ENCODER_CLK, GPIO_IRQ_EDGE_FALL | GPIO_IRQ_EDGE_RISE, true);
+    gpio_set_irq_enabled_with_callback(ENCODER_CLK, GPIO_IRQ_EDGE_FALL | GPIO_IRQ_EDGE_RISE, true, &encoder_callback);
+    gpio_set_irq_enabled(ENCODER_DT, GPIO_IRQ_EDGE_FALL | GPIO_IRQ_EDGE_RISE, true);
+    gpio_set_irq_enabled(ENCODER_SWITCH, GPIO_IRQ_EDGE_FALL | GPIO_IRQ_EDGE_RISE, true);
 
     // LED
     gpio_init(PICO_DEFAULT_LED_PIN);
     gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
 
-    //blink();
+    // blink();
 
     // If you don't do anything before initializing a display pi pico is too fast and starts sending
     // commands before the screen controller had time to set itself up, so we add an artificial delay for
@@ -186,27 +177,28 @@ int main()
     // Available fonts are listed in textRenderer's readme
     // Last we tell this function where to anchor the text
     // Anchor means top left of what we draw
-    std::array<int, 2> rows = { 0, 34 };
+    std::array<int, 2> rows = { 3, 34 };
 
     uint32_t currentDigit = 6;
+    uint32_t x_offset = 4;
 
     auto drawDisplay = [&] {
         // Name of band
         display.clear();
 
-        drawRect(&display, 0, 0, 127, 63);
+        // drawRect(&display, 0, 0, 127, 63);
 
-        drawText(&display, font_12x16, "41 Meter", 0, 0);
+        drawText(&display, font_12x16, "41 metre", x_offset, 2);
 
         // Frequency
-        auto str = std::to_string(value) + "Mhz";
-        drawText(&display, font_12x16, str.c_str(), 0, rows[1]);
+        auto str = std::to_string(frequency) + "Mhz";
+        drawText(&display, font_12x16, str.c_str(), x_offset, rows[1]);
 
         // Underline for the current counter digit to change
         const uint32_t fontHeight = 16;
         const uint32_t fontWidth = 12;
         uint32_t pad = 1;
-        fillRect(&display, (currentDigit * fontWidth) + pad, rows[1] + fontHeight, ((currentDigit + 1) * fontWidth), rows[1] + fontHeight + 2);
+        fillRect(&display, (currentDigit * fontWidth) + pad + x_offset, rows[1] + fontHeight, ((currentDigit + 1) * fontWidth) + x_offset, rows[1] + fontHeight + 2);
 
         // Send buffer to the display
         display.sendBuffer();
@@ -217,17 +209,17 @@ int main()
     {
         // When the encoder ticks, advance
         bool update_clock = false;
-        bool update_display = true;
+        bool update_display = false;
 
         if (abs(encoder_count) > 2)
         {
-            auto count = encoder_count / 2;
-            value += (count * pow(10, (6 - currentDigit)));
+            auto count = -encoder_count / 2;
+            frequency += (count * pow(10, (6 - currentDigit)));
             encoder_count = 0;
             update_clock = true;
             update_display = true;
 
-            value = std::clamp(value.load(), 7000000ull, 7200000ull);
+            frequency = std::clamp(frequency, 7000000ull, 7200000ull);
         }
 
         // Encoder button pressed, choose the next unit to change
@@ -245,21 +237,17 @@ int main()
         // Update the clock
         if (update_clock)
         {
-            // si5351_set_freq(value * 100ULL, SI5351_CLK0);
+            // si5351_set_freq(frequency * 100ULL, SI5351_CLK0);
         }
 
         // Update the display
         if (update_display)
         {
-            if (value.load() < 7000020)
-            {
-                value++;
-            }
             drawDisplay();
         }
 
         // Back off, just a bit
-        sleep_ms(1);
+        //sleep_ms(1);
     }
 
     reset_usb_boot(0, 0);
